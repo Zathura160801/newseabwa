dacf45a167973ff369a763560f436357264f7829
Zathura160801 <adipz160801@gmail.com>
2024-12-30 12:59:03 +0700

    setup crud subscription group

--- app/Filament/Resources/SubscriptionGroupResource.php
+++ app/Filament/Resources/SubscriptionGroupResource.php
@@ -2,16 +2,31 @@
 
 namespace App\Filament\Resources;
 
+use Filament\Forms;
+use Filament\Tables;
+use App\Models\Product;
+use Filament\Forms\Form;
+use Filament\Tables\Table;
+use Filament\Resources\Resource;
+use App\Models\SubscriptionGroup;
+use Filament\Forms\Components\Select;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+use Filament\Tables\Columns\ImageColumn;
+use Illuminate\Database\Eloquent\Builder;
+use Filament\Tables\Filters\TrashedFilter;
+use Filament\Tables\Actions\BulkActionGroup;
+use Filament\Tables\Actions\DeleteBulkAction;
+use Filament\Tables\Actions\RestoreBulkAction;
+use Filament\Tables\Actions\ForceDeleteBulkAction;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
 use App\Filament\Resources\SubscriptionGroupResource\Pages;
 use App\Filament\Resources\SubscriptionGroupResource\RelationManagers;
-use App\Models\SubscriptionGroup;
-use Filament\Forms;
-use Filament\Forms\Form;
-use Filament\Resources\Resource;
-use Filament\Tables;
-use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\SubscriptionGroupResource\Pages\EditSubscriptionGroup;
+use App\Filament\Resources\SubscriptionGroupResource\Pages\ListSubscriptionGroups;
+use App\Filament\Resources\SubscriptionGroupResource\Pages\CreateSubscriptionGroup;
 
 class SubscriptionGroupResource extends Resource
 {
@@ -21,29 +36,86 @@ class SubscriptionGroupResource extends Resource
 
     public static function form(Form $form): Form
     {
-        return $form
-            ->schema([
-                //
-            ]);
+        return $form->schema([
+            Select::make('product_id')
+                ->relationship('product', 'name')
+                ->searchable()
+                ->preload()
+                ->required()
+                ->live()
+                ->afterStateUpdated(function ($state, callable $set) {
+                    $product = Product::find($state);
+                    $max_capacity = $product ? $product->capacity : 0;
+
+                    $set('max_capacity', $max_capacity);
+                })
+                ->afterStateHydrated(function (callable $get, callable $set, $state) {
+                    $productId = $state;
+                    if ($productId) {
+                        $product = Product::find($productId);
+                        $max_capacity = $product ? $product->capacity : 0;
+
+                        $set('max_capacity', $max_capacity);
+                    }
+                }),
+                
+            TextInput::make('max_capacity')
+                ->required()
+                ->label('Max Capacity')
+                ->readOnly()
+                ->numeric()
+                ->prefix('People'),
+
+            TextInput::make('participant_count')
+                ->required()
+                ->label('Total Capacity')
+                ->numeric()
+                ->prefix('People'),
+
+            Select::make('product_subscription_id')
+                ->relationship('productSubscription', 'booking_trx_id')
+        ]);
     }
 
     public static function table(Table $table): Table
     {
         return $table
             ->columns([
-                //
+                ImageColumn::make('product.thumbnail')
+                    ->label('Photo'),
+
+                TextColumn::make('productSubscription.booking_trx_id')
+                    ->label('Booking ID')
+                    ->searchable(),
+
+                TextColumn::make('id')
+                    ->label('Group ID')
+                    ->searchable(),
+
+                TextColumn::make('participant_count'),
+
+                TextColumn::make('max_capacity'),
+
+                IconColumn::make('is_full')
+                    ->label('full')
+                    ->boolean()
+                    ->getStateUsing(fn ($record) => $record->participant_count >= $record->max_capacity)
+                    ->trueIcon('heroicon-o-check-circle')
+                    ->falseIcon('heroicon-o-x-circle')
+                    ->trueColor('success')
+                    ->falseColor('danger'),
             ])
             ->filters([
-                Tables\Filters\TrashedFilter::make(),
+                TrashedFilter::make(),
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
+                EditAction::make(),
             ])
             ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                    Tables\Actions\ForceDeleteBulkAction::make(),
-                    Tables\Actions\RestoreBulkAction::make(),
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                    ForceDeleteBulkAction::make(),
+                    RestoreBulkAction::make(),
                 ]),
             ]);
     }
